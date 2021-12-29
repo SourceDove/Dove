f56441f43b4523b628d171d637e2765104265311
SourceDove <95582871+SourceDove@users.noreply.github.com>
2021-12-29 21:56:40 +0300

    Update Dove.lua

--- Dove.lua
+++ Dove.lua
@@ -2089,7 +2089,7 @@ Text = [[⌔︰Hello buttons at the bottom to delete social media accounts .]]
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '⌔ Delete bot',url="https://t.me/R3J3BOT"}},
-{{text = '⌔ Delete facebook',url="https://www.facebook.com/help/deleteaccount"},{text = '⌔ Delete instagram',url="https://www.instagram.com/accounts/login/?next=/accounts/remove/request/permanent"}},{{text= '⌔ Dove Source',url="https://t.me/EEPEF"}}
+{{text = '⌔ Delete face',url="https://www.facebook.com/help/deleteaccount"},{text = '⌔ Delete insta',url="https://www.instagram.com/accounts/login/?next=/accounts/remove/request/permanent"}},{{text= '⌔ Dove Source',url="https://t.me/EEPEF"}}
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/R3J3BOT&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
